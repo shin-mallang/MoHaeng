6de9b3487fb7b9b50c8fd5e194c408f2a38800ea
shinD <huipulci1@naver.com>
2022-12-24 02:35:25 +0900

    feat: 회원가입 컨트롤러 구현 (#2)

---
+++ mohaeng/src/main/java/com/mohaeng/presentation/api/member/MemberRestController.java
@@ -0,0 +1,33 @@
+package com.mohaeng.presentation.api.member;
+
+import com.mohaeng.domain.member.usecase.SignUpUseCase;
+import com.mohaeng.presentation.api.member.request.SignUpRequest;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path = "/api")
+public class MemberRestController {
+
+    private final SignUpUseCase signUpUseCase;
+
+    public MemberRestController(final SignUpUseCase signUpUseCase) {
+        this.signUpUseCase = signUpUseCase;
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping(path = "/v1/sign-up")
+    public void signUp(
+            @Valid @RequestBody final SignUpRequest signUpRequest
+    ) {
+        signUpUseCase.command(
+                new SignUpUseCase.Command(
+                        signUpRequest.username(),
+                        signUpRequest.password(),
+                        signUpRequest.name(),
+                        signUpRequest.age(),
+                        signUpRequest.gender())
+        );
+    }
+}
---
+++ mohaeng/src/main/java/com/mohaeng/presentation/config/aop/ApiRestControllerAdvice.java
@@ -0,0 +1,58 @@
+package com.mohaeng.presentation.config.aop;
+
+import com.mohaeng.domain.member.exception.DuplicateUsernameException;
+import com.mohaeng.presentation.config.ErrorResponseDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import static java.util.stream.Collectors.joining;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.CONFLICT;
+
+@RestControllerAdvice
+public class ApiRestControllerAdvice {
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    /**
+     * 400
+     */
+    @ResponseStatus(BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    ErrorResponseDto handleException(MethodArgumentNotValidException e) {
+        BindingResult result = e.getBindingResult();
+        String fields = result.getFieldErrors()
+                .stream()
+                .map(FieldError::getField)
+                .collect(joining(", "));
+        log.error("필드들이 입력되지 않았습니다. [%s]".formatted(fields));
+        return new ErrorResponseDto(BAD_REQUEST.name(), "입력되지 않은 필드가 있습니다. [%s]".formatted(fields));
+    }
+
+    /**
+     * 400
+     */
+    @ResponseStatus(BAD_REQUEST)
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    ErrorResponseDto handleException(HttpMessageNotReadableException e) {
+        log.error(e.getMessage());
+        return new ErrorResponseDto(BAD_REQUEST.name(), "ENUM 매핑 시 오류 발생");
+    }
+
+    /**
+     * 409
+     */
+    @ResponseStatus(CONFLICT)
+    @ExceptionHandler(DuplicateUsernameException.class)
+    ErrorResponseDto handleException(DuplicateUsernameException e) {
+        log.error(e.getMessage());
+        return new ErrorResponseDto(CONFLICT.name(), e.getMessage());
+    }
+}
