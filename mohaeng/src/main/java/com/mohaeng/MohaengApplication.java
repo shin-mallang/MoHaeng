b28cb0f7c48ce1cc2d5f65e66f66e6c13335ae0e
shinD <huipulci1@naver.com>
2022-12-23 22:09:09 +0900

    chore: 프로젝트 세팅

---
+++ mohaeng/src/main/java/com/mohaeng/MohaengApplication.java
@@ -0,0 +1,13 @@
+package com.mohaeng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MohaengApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MohaengApplication.class, args);
+	}
+
+}
