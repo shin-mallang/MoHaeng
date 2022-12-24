71291f77935ccb9074585de7b55a73de8029a79c
shinD <huipulci1@naver.com>
2022-12-24 15:31:25 +0900

    feat: JWT properties 구성 (#3)

--- mohaeng/src/main/java/com/mohaeng/MohaengApplication.java
+++ mohaeng/src/main/java/com/mohaeng/MohaengApplication.java
@@ -2,7 +2,9 @@ package com.mohaeng;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+@ConfigurationPropertiesScan
 @SpringBootApplication
 public class MohaengApplication {
 
---
+++ mohaeng/src/main/java/com/mohaeng/common/properties/JwtProperties.java
@@ -0,0 +1,25 @@
+package com.mohaeng.common.properties;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.bind.ConstructorBinding;
+
+@ConfigurationProperties("jwt")
+public class JwtProperties {
+
+    private String secretKey;
+    private Long accessTokenExpirationPeriodDay;
+
+    @ConstructorBinding
+    public JwtProperties(final String secretKey, final Long accessTokenExpirationPeriodDay) {
+        this.secretKey = secretKey;
+        this.accessTokenExpirationPeriodDay = accessTokenExpirationPeriodDay;
+    }
+
+    public String secretKey() {
+        return secretKey;
+    }
+
+    public Long accessTokenExpirationPeriodDay() {
+        return accessTokenExpirationPeriodDay;
+    }
+}
