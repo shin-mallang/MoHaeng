d7211eeddfc49fb95fee9fd54f4bf755374add93
shinD <huipulci1@naver.com>
2023-01-02 23:19:18 +0900

    feat: Club 생성 컨트롤러 구현 (#14)

---
+++ mohaeng/src/main/java/com/mohaeng/presentation/api/club/mapper/ClubPersistenceMapper.java
@@ -0,0 +1,11 @@
+package com.mohaeng.presentation.api.club.mapper;
+
+import com.mohaeng.application.club.usecase.CreateClubUseCase;
+import com.mohaeng.presentation.api.club.CreateClubRestController;
+
+public class ClubPersistenceMapper {
+
+    public static CreateClubUseCase.Command toApplicationDto(final CreateClubRestController.CreateClubRequest request) {
+        return null;
+    }
+}
