5c127764a363787b07c83363105da9d9a9b02018
shinD <huipulci1@naver.com>
2023-01-02 23:31:09 +0900

    test: add study test

---
+++ mohaeng/src/test/java/com/stucy/InterfaceFinalParameterTest.java
@@ -0,0 +1,31 @@
+package com.stucy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class InterfaceFinalParameterTest {
+
+
+    @Test
+    @DisplayName("final이 붙은 않은 파라미터를 가진 인터페이스 메서드의 구현체가 final을 붙이지 않고 구현하여 그 값을 변경하는 경우에 대한 테스트")
+    void test() {
+        // given
+        TestInterfaceImpl testInterface = new TestInterfaceImpl();
+
+        // when
+        testInterface.finalMethod(20);
+        // 문제 없다.
+    }
+
+    interface TestInterface {
+        void finalMethod(final int a);
+    }
+
+    class TestInterfaceImpl implements TestInterface {
+        @Override
+        public void finalMethod(int a) {
+            a = 10;
+            a = 20;
+        }
+    }
+}
