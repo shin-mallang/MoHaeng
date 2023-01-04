701430727bb25067a797fc3e165ceaaabacde67b
shin-mallang <huipulci1@naver.com>
2023-01-04 22:26:52 +0900

    fix: JpaClubRoleRepository에 findAll 구현 추가 (#40)
    기존에는 오류가 났었고, ClubRoleRepository의 saveAll()이 List 대신 Iterable을 받게 하자 타입 이레이저에 의한 오류가 발생하였음.
    이를 해결하기 위해 이것저것 수정하던 중 default 메서드로 Override 한 무한재귀에 빠지지 않고 ListCrudRepository의 saveAll을 부르도록 캐스팅 코드 추가하여 해결.

--- mohaeng/src/main/java/com/mohaeng/infrastructure/persistence/database/repository/club/role/JpaClubRoleRepository.java
+++ mohaeng/src/main/java/com/mohaeng/infrastructure/persistence/database/repository/club/role/JpaClubRoleRepository.java
@@ -4,5 +4,12 @@ import com.mohaeng.domain.club.model.role.ClubRole;
 import com.mohaeng.domain.club.repository.role.ClubRoleRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface JpaClubRoleRepository extends JpaRepository<ClubRole, Long>, ClubRoleRepository {
+
+    @Override
+    default List<ClubRole> saveAll(List<ClubRole> entities) {
+        return saveAll((Iterable<ClubRole>) entities);
+    }
 }
