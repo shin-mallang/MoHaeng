6f681d594092396a239b74d03988077f033dbc58
shin-mallang <huipulci1@naver.com>
2023-02-22 14:46:38 +0900

    feat: 기본 역할 변경 컨트롤러 구현 (#125)

---
+++ mohaeng/src/main/java/com/mohaeng/club/club/presentation/ChangeDefaultRoleController.java
@@ -0,0 +1,32 @@
+package com.mohaeng.club.club.presentation;
+
+import com.mohaeng.authentication.presentation.argumentresolver.Auth;
+import com.mohaeng.club.club.application.usecase.ChangeDefaultRoleUseCase;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ChangeDefaultRoleController {
+
+    public static final String CHANGE_DEFAULT_ROLE_URL = "/api/club/{clubId}/club-role/{clubRoleId}/make-default";
+
+    private final ChangeDefaultRoleUseCase changeDefaultRoleUseCase;
+
+    public ChangeDefaultRoleController(final ChangeDefaultRoleUseCase changeDefaultRoleUseCase) {
+        this.changeDefaultRoleUseCase = changeDefaultRoleUseCase;
+    }
+
+    @PostMapping(CHANGE_DEFAULT_ROLE_URL)
+    public ResponseEntity<Void> changeDefaultRole(
+            @PathVariable(name = "clubId") final Long clubId,
+            @PathVariable(name = "clubRoleId") final Long clubRoleId,
+            @Auth final Long memberId
+    ) {
+        changeDefaultRoleUseCase.command(
+                new ChangeDefaultRoleUseCase.Command(memberId, clubId, clubRoleId)
+        );
+        return ResponseEntity.ok().build();
+    }
+}
