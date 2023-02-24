eec851d4a89ccb53de676223dd68cdd66dd6e296
shin-mallang <huipulci1@naver.com>
2023-02-24 21:11:37 +0900

    fix: 모임 조회 경로를 LoginInterceptor 대상에서 제외 (#144)

--- mohaeng/src/main/java/com/mohaeng/authentication/presentation/config/AuthenticationConfig.java
+++ mohaeng/src/main/java/com/mohaeng/authentication/presentation/config/AuthenticationConfig.java
@@ -2,16 +2,19 @@ package com.mohaeng.authentication.presentation.config;
 
 import com.mohaeng.authentication.presentation.argumentresolver.AuthArgumentResolver;
 import com.mohaeng.authentication.presentation.interceptor.LogInInterceptor;
+import com.mohaeng.common.interceptor.PathAndMethodMatcherInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
+import java.util.Set;
 
 import static com.mohaeng.authentication.presentation.LogInController.LOGIN_URL;
 import static com.mohaeng.club.club.presentation.query.QueryClubByIdController.QUERY_CLUB_BY_ID_URL;
 import static com.mohaeng.member.presentation.SignUpController.SIGN_UP_URL;
+import static org.springframework.http.HttpMethod.GET;
 
 @Configuration
 public class AuthenticationConfig implements WebMvcConfigurer {
@@ -27,9 +30,14 @@ public class AuthenticationConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(final InterceptorRegistry registry) {
-        registry.addInterceptor(logInInterceptor)
-                .addPathPatterns("/api/**")
-                .excludePathPatterns(SIGN_UP_URL, LOGIN_URL, QUERY_CLUB_BY_ID_URL);
+        PathAndMethodMatcherInterceptor interceptor = new PathAndMethodMatcherInterceptor(logInInterceptor);
+        interceptor
+                .addPathPatterns(Set.of("/api/**"))
+                .excludePathPattern(Set.of(SIGN_UP_URL, LOGIN_URL))
+                .excludePathPattern(Set.of(QUERY_CLUB_BY_ID_URL), Set.of(GET));
+
+        // 모든 경로에 대해 매핑된다
+        registry.addInterceptor(interceptor);
     }
 
     @Override
