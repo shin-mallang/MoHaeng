f644e385937370053cafa80c5cd58af8d29155c8
shin-mallang <huipulci1@naver.com>
2023-02-27 10:27:32 +0900

    chore: 주석 내용 수정 (#60)

--- mohaeng/src/main/java/com/mohaeng/notification/application/usecase/query/QueryFillOutApplicationFormNotificationUseCase.java
+++ mohaeng/src/main/java/com/mohaeng/notification/application/usecase/query/QueryFillOutApplicationFormNotificationUseCase.java
@@ -7,9 +7,9 @@ import org.springframework.data.domain.Pageable;
 public interface QueryFillOutApplicationFormNotificationUseCase {
 
     /**
-     * 가입 신청 요청 알림은, 읽음 처리되지 않는다.
-     * <p>
-     * 오직 수락 혹은 거절을 통해서만 제거된다.
+     * 가입 신청 요청 알림은 읽음 여부에 관계없이 존재하면 모두 조회한다.
+     * (존재한다는 것은 아직 처리되지 않았다는 상태이다)
+     * 이는 오직 수락 혹은 거절을 통해서만 제거된다.
      */
     Page<FillOutApplicationFormNotificationDto> query(final Query query);
 
